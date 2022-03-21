4bf5af73e739026e1add6c30e8cc32262dadec6f
KeerthanKumar Jojoda <keerthan.j@westagilelabs.com>
2022-03-21 10:47:48 +0530

    Added node and react folders

--- Node/routes/forum.js
+++ Node/routes/forum.js
@@ -15,7 +15,7 @@ router.post("/", (req, res) => {
   console.log(req.body);
   let { title, author, dateOfAdding, body } = req.body;
   forums.push({ title, author, dateOfAdding, body });
-  res.send({ status: "New user details are added" });
+  res.send({ status: "New book details are added" });
 });
 router.delete("/:id", (req, res) => {
   console.log(req.params.id);
@@ -27,11 +27,11 @@ router.delete("/:id", (req, res) => {
     }
   });
   forums = newForums;
-  res.send({ status: "Selected user details are deleted" });
+  res.send({ status: "Selected book details are deleted" });
 });
 router.get("/clearall", (req, res) => {
   forums = [];
-  res.send({ status: "All user details are removed" });
+  res.send({ status: "All book details are removed" });
 });
 
 module.exports = router;
